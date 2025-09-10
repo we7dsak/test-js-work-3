330441e1093f18be1a3940e4f72b49f57c276b8a
we7dsak <we7laptop@gmail.com>
2025-09-10 17:05:44 +0300

    completed

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+function filterArray(numbers, value) {
+    const newarr = []
+   
+    for (let index = 0; index < numbers.length; index++) {
+    
+        if (numbers[index] > value) {
+            newarr.push(numbers[index])
+        }
+    }
+return newarr
+
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
